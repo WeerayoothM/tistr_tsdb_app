05103be9067ba76fe8494affafe73b6071089cc4
AofPatiphan <patiphan.ma@hotmail.com>
2023-08-10 00:26:06 +0700

    fix login admin

--- web/src/stores/user/APIS/index.ts
+++ web/src/stores/user/APIS/index.ts
@@ -4,6 +4,6 @@ import camelcaseKeys from "camelcase-keys";
 const apiEndPoint = import.meta.env.VITE_API_ENDPOINT;
 
 export const userLogin = async (payload: any) => {
-  const response = await axios.post(`${apiEndPoint}/login`, payload);
+  const response = await axios.post(`${apiEndPoint}/loginAdmin`, payload);
   return camelcaseKeys(response, { deep: true });
 };
